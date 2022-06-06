246d8bff787e39dba6cceadfc5b97186ddbe8196
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-06-06 15:38:12 -0500

    main.bundle respondes with a 404 error

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,14 +47,14 @@ $(document).ready(function() {
     {
       title: "We travel all over the US",
       subtitle: "Check out our schedule!",
-      img: "./assets/img/food-table-min.jpg",
+      img: "./assets/img/food-table.jpg",
       btnText: "View Schedule",
       btnUrl: "schedule.html"
     },
     {
       title: "Our food is seriously the bomb!",
       subtitle: "What are you waiting for?",
-      img: "./assets/img/grill-min.jpg",
+      img: "./assets/img/grill.jpg",
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html"
     },
